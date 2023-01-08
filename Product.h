da9c43af0aba46bf68c6b9cfcca410d59b19a335
Scarpy019 <Scarpy019@gmail.com>
2023-01-08 21:23:01 +0200

    Made default constructor callable

--- Product.h
+++ Product.h
@@ -15,7 +15,7 @@ public:
 
     unsigned int unitsSold;
 
-    Product(const char *name, unsigned int price=0, unsigned int unitsAvailable=0, unsigned int unitsSold=0);
+    Product(const char *name="THIS SHOULDN'T BE VISIBLE", unsigned int price=0, unsigned int unitsAvailable=0, unsigned int unitsSold=0);
     ~Product();
 
     static void writeProducts(const std::map<std::string,Product>& products);
